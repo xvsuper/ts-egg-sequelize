6e8773df88435df941832ec485e298b422ce2717
vsuper <xvsuper@163.com>
2019-07-23 23:23:39 +0800

    发布1.1.0

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# 测试中暂不可用
+# ts-egg-sequelize
++ 此插件根据官方 egg-sequelize@5.2.0 修改
++ 依赖 [typescript-sequelize](https://www.npmjs.com/package/sequelize-typescript) 插件（以包含在本插件无需手动安装）
+
+## 版本说明
++ 本插件适用 sequelize V4 及以下版本
+
+## 配置
+
+### 配置插件（plugin.ts）
+```js
+sequelize: {
+    enable: true,
+    package: 'ts-egg-sequelize',
+}
+```
+
+### 配置数据库跟egg-sequelize没区别（config.local.ts）
+```js
+config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'egg',
+}
+```
+
+## 使用
+
+### 示例
+```ts
+import { Table, Column, Model, Unique, CreatedAt, UpdatedAt } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'users',
+})
+export default class User extends Model<User> {
+
+  @Column({
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+
+  @Unique
+  @Column
+  uid: string;
+
+  @Unique
+  @Column
+  username: string;
+
+  @Column
+  password: string;
+
+  @Unique
+  @Column
+  phone_code: number;
+
+  @Column
+  status: number;
+
+  @CreatedAt
+  @Column
+  created_at: Date;
+
+  @UpdatedAt
+  @Column
+  updated_at: Date;
+
+}
+```
+
+详细用法及函数请看这里：[egg-sequelize](https://www.npmjs.com/package/sequelize-typescript)
--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,44 @@
-import * as sequelize from "sequelize-typescript";
+// import { Model, Sequelize, ISequelizeConfig } from 'sequelize-typescript';
+import { Model, ISequelizeConfig } from '../../sso/service/node_modules/sequelize-typescript';
 
-interface EggSequelizeOptions extends sequelize.Options {
+interface EggSequelizeOptions extends ISequelizeConfig {
+  /**
+   * load all models to `app[delegate]` and `ctx[delegate]`, default to `model`
+   */
   delegate?: string;
+
+  /**
+   * load models from `app/model/*.js`
+   */
   baseDir?: string;
+
+  /**
+   * ignore `app/${baseDir}/index.js` when load models, support glob and array
+   */
+  exclude?: string | Array<string>;
+
+  /**
+   * A full database URI
+   * @example
+   * `connectionUri:"mysql://localhost:3306/database"`
+   */
+  connectionUri?: string;
 }
 
 interface DataSources {
-  [datasources]: EggSequelizeOptions;
+  datasources: EggSequelizeOptions[];
 }
 
 declare module 'egg' {
 
   // extend app
   interface Application {
-    Sequelize: sequelize.SequelizeStatic;
-    model: sequelize.Model;
+    model: typeof Model;
   }
 
   // extend context
   interface Context {
-    model: sequelize.Model;
+    model: typeof Model;
   }
 
   // extend your config
